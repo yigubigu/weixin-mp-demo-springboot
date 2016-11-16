53d1729a6ca15151a4aef44c7c9282867c0c0c97
lory <lory.hou@augmentum.com>
2016-11-16 13:43:53 +0800

    add tag

--- src/main/java/com/github/binarywang/demo/wechat/controller/FollowerController.java
+++ src/main/java/com/github/binarywang/demo/wechat/controller/FollowerController.java
@@ -1,5 +1,8 @@
 package com.github.binarywang.demo.wechat.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import me.chanjar.weixin.common.exception.WxErrorException;
 import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.bean.result.WxMpUserList;
@@ -7,6 +10,7 @@ import me.chanjar.weixin.mp.bean.result.WxMpUserList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -29,8 +33,20 @@ public class FollowerController {
 			WxMpUserList wxMpUserList = this.wxService.getUserService().userList(null);
 			return new ResponseWrapper(wxMpUserList);
 		} catch (WxErrorException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.error(e.getMessage());
+		}
+		return new ResponseWrapper();
+	}
+	
+	@RequestMapping(value="batchAssignTag", method=RequestMethod.POST)
+	@ResponseBody()
+	public ResponseWrapper batchAssginTagToUsers(@RequestBody Map<String, Object> params) {
+		String tag = (String)params.get("tags");
+		List<String> userIds = (List<String>) params.get("userIds");
+		try {
+			this.wxService.getUserService().userUpdateRemark(userIds.get(0), tag);
+		} catch (WxErrorException e) {
+			logger.error(e.getMessage());
 		}
 		return new ResponseWrapper();
 	}
